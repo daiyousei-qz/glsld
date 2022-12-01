c604aed25b2922a0e92a04c3409f732b3a1934f9
daiyousei-qz <qyzheng2@outlook.com>
2022-11-30 21:57:16 -0800

    workaround deleting AST node on destruction

--- glsl-fe/include/AstContext.h
+++ glsl-fe/include/AstContext.h
@@ -10,6 +10,25 @@ namespace glsld
     public:
         friend class Parser;
 
+        ~AstContext()
+        {
+            for (auto node : nodes) {
+                switch (node->GetTag()) {
+#define DECL_AST_BEGIN_BASE(TYPE)
+#define DECL_AST_TYPE(TYPE)                                                                                            \
+    case AstNodeTag::TYPE:                                                                                             \
+        delete static_cast<TYPE*>(node);
+#define DECL_AST_END_BASE(TYPE)
+#include "GlslAst.inc"
+#undef DECL_AST_BEGIN_BASE
+#undef DECL_AST_TYPE
+#undef DECL_AST_END_BASE
+                default:
+                    GLSLD_UNREACHABLE();
+                }
+            }
+        }
+
         auto AddGlobalDecl(AstDecl* decl) -> void
         {
             globalDecls.push_back(decl);
@@ -33,5 +52,8 @@ namespace glsld
         std::vector<AstDecl*> globalDecls;
         std::vector<AstVariableDecl*> variableDecls;
         std::vector<AstFunctionDecl*> functionDecls;
+
+        // FIXME: optimize memory layout
+        std::vector<AstNodeBase*> nodes;
     };
 } // namespace glsld
