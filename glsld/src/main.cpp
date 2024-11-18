2e5abacfd995dd3a0ffec0ec7e35b895bf1dcacd
daiyousei-qz <qyzheng2@outlook.com>
2024-11-18 21:02:20 +0800

    Add a simple mechanism to dump on crash

--- glsld/src/main.cpp
+++ glsld/src/main.cpp
@@ -5,6 +5,47 @@
 #include <windows.h>
 #endif
 
+#if defined(GLSLD_DEBUG) && defined(GLSLD_OS_WIN)
+#include <DbgHelp.h>
+#pragma comment(lib, "Dbghelp.lib")
+
+LONG WINAPI ExceptionFilter(EXCEPTION_POINTERS* pExceptionPointers)
+{
+    wchar_t pathBuffer[MAX_PATH];
+    GetModuleFileNameW(nullptr, pathBuffer, MAX_PATH);
+
+    wchar_t* lastSlash = wcsrchr(pathBuffer, L'\\');
+    if (lastSlash) {
+        *(lastSlash + 1) = L'\0'; // Keep the trailing backslash
+    }
+
+    SYSTEMTIME time;
+    GetLocalTime(&time);
+    wchar_t dateTimeBuffer[64];
+    swprintf(dateTimeBuffer, 64, L"minidump-%04d%02d%02d-%02d%02d%02d.dmp", time.wYear, time.wMonth, time.wDay,
+             time.wHour, time.wMinute, time.wSecond);
+    wcscat_s(pathBuffer, MAX_PATH, dateTimeBuffer);
+
+    HANDLE hFile = CreateFileW(pathBuffer, GENERIC_WRITE, 0, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
+    if (hFile != INVALID_HANDLE_VALUE) {
+        MINIDUMP_EXCEPTION_INFORMATION mdei;
+        mdei.ThreadId          = GetCurrentThreadId();
+        mdei.ExceptionPointers = pExceptionPointers;
+        mdei.ClientPointers    = FALSE;
+
+        MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(), hFile, MiniDumpWithDataSegs, &mdei, nullptr,
+                          nullptr);
+        CloseHandle(hFile);
+    }
+    return EXCEPTION_EXECUTE_HANDLER;
+}
+
+void SetUnhandledExceptionFilter()
+{
+    ::SetUnhandledExceptionFilter(ExceptionFilter);
+}
+#endif
+
 namespace glsld
 {
     cl::Opt<bool> WaitDebugger(
@@ -46,7 +87,11 @@ namespace glsld
 
 auto main(int argc, char* argv[]) -> int
 {
+#if defined(GLSLD_DEBUG) && defined(GLSLD_OS_WIN)
+    SetUnhandledExceptionFilter();
+#endif
     glsld::cl::ParseArguments(argc, argv);
+    throw 0;
     glsld::DoMain();
     return 0;
 }
