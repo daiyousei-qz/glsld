2d27d18aadec9479c1bfff56d2e73b16fd43e108
Daiyousei <12378019+daiyousei-qz@users.noreply.github.com>
2025-11-09 02:19:54 +0800

    Add AppVersion header and update argument parser to include versioning (#31)

---
+++ share/AppVersion.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace glsld
+{
+    inline constexpr int GlsldVersionMajor = 0;
+    inline constexpr int GlsldVersionMinor = 2;
+    inline constexpr int GlsldVersionPatch = 1;
+} // namespace glsld
