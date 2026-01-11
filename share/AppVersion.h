72ba2ef8f2348e779b916f85fe22bc1789d6401f
daiyousei-qz <qyzheng2@outlook.com>
2026-01-11 20:09:54 +0800

    Bump version to 0.3.0

--- share/AppVersion.h
+++ share/AppVersion.h
@@ -3,6 +3,6 @@
 namespace glsld
 {
     inline constexpr int GlsldVersionMajor = 0;
-    inline constexpr int GlsldVersionMinor = 2;
-    inline constexpr int GlsldVersionPatch = 1;
+    inline constexpr int GlsldVersionMinor = 3;
+    inline constexpr int GlsldVersionPatch = 0;
 } // namespace glsld
